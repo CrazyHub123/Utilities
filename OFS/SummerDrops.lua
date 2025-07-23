cc9a8e2efe0f555bccf3828fc5ad871cb2404980
CrazyHub <78809386+CrazyHub123@users.noreply.github.com>
2025-07-23 16:07:14 +0300

    Create SummerDrops.lua

---
+++ OFS/SummerDrops.lua
@@ -0,0 +1,24 @@
+local v24 = {
+	"SMedal",
+	"TMedal",
+	"SoulEssence",
+	"ReviveEssence",
+	"MagmaEssence",
+	"RobEssence",
+	"GomuEssence",
+	"LMedal",
+	"YMedal",
+	"G5Essence",
+	"TGMedal",
+	"PrimordialEssence",
+	"RSHat",
+	"RSCape",
+	"RSFit",
+	"BSHat",
+	"BSFit",
+	"TTFit",
+	"TTCape",
+	"TTHat"
+}
+
+return v24
